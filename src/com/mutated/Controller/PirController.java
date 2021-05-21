11af8006d740b98524b77237cd12703ba2c6e1a9
mike ehart <michaelt.ehart@gmail.com>
2021-05-20 18:30:57 -0700

    pir controller class fix

--- src/com/mutated/Controller/PirController.java
+++ src/com/mutated/Controller/PirController.java
@@ -8,22 +8,25 @@ public class PirController {
 
     private int sensorTripCounter;
     private String newResult;
+    private String result;
     private TriggerController trigger;
 
     public PirController() {
         sensorTripCounter = 0;
         newResult = "0";
+        result = "0";
         trigger = new TriggerController();
     }
 
 
     public void pirSensorTrip(Runtime runtime) throws InterruptedException, IOException {
 
-        InputStream input = runtime.exec("gpio read 2").getInputStream();
-        Scanner s = new Scanner(input).useDelimiter("\\A");
-        String result = s.hasNext() ? s.next() : "0";
 
         while (true) {
+
+            InputStream input = runtime.exec("gpio read 2").getInputStream();
+            Scanner s = new Scanner(input).useDelimiter("\\A");
+            result = s.hasNext() ? s.next() : "0";
 
             if (!newResult.equals(result)) {
                 if (sensorTripCounter > 0) {
